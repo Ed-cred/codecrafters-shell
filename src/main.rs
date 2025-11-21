15b5e36f60f5559f874d5680d2e4a9fb6b7bb4b6
Edward <edwardandrew63@gmail.com>
2025-11-21 21:12:02 +0200

    command not found

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,13 @@ use std::io::{self, Write};
 
 fn main() {
     print!("$ ");
+    let mut buf = String::new();
+    match io::stdin().read_line(&mut buf) {
+        Ok(_) => {
+            let command = buf.trim_end();
+            println!("{}: command not found", command)
+        }
+        Err(e) => println!("error {e}"),
+    }
     io::stdout().flush().unwrap();
 }
