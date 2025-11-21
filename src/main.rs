2f55468534196aac9361c589e96b78c64d4bc668
Edward <edwardandrew63@gmail.com>
2025-11-21 22:01:33 +0200

    type builtin

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,25 @@
 use std::io::{self, Write};
 
 fn main() {
+    let builtins: [&str; 3] = ["exit", "echo", "type"];
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
         let mut buf = String::new();
-        match io::stdin().read_line(&mut buf) {
-            Ok(_) => {
-                let input = buf.trim_end();
-                let (command, rest) = input.split_once(' ').unwrap_or((input, ""));
-                match command {
-                    "exit" => return,
-                    "echo" => println!("{}", rest),
-                    _ => println!("{}: command not found", command),
+        let _ = io::stdin().read_line(&mut buf).unwrap();
+        let buf = buf.trim_end();
+        let (command, rest) = buf.split_once(' ').unwrap_or((buf, ""));
+        match command {
+            "exit" => return,
+            "echo" => println!("{}", rest),
+            "type" => {
+                if builtins.contains(&rest) {
+                    println!("{rest} is a shell builtin");
+                } else {
+                    println!("{}: command not found", rest);
                 }
             }
-            Err(e) => println!("error {e}"),
+            _ => println!("{}: command not found", command),
         }
     }
 }
