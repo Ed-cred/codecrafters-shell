5c86c231b7864f1b2cb8872d50ff361d32f9ff7a
Edward <edwardandrew63@gmail.com>
2025-11-21 21:16:07 +0200

    infinte loops

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 use std::io::{self, Write};
 
 fn main() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
-    let mut buf = String::new();
-    match io::stdin().read_line(&mut buf) {
-        Ok(_) => {
-            let command = buf.trim_end();
-            println!("{}: command not found", command)
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+        let mut buf = String::new();
+        match io::stdin().read_line(&mut buf) {
+            Ok(_) => {
+                let command = buf.trim_end();
+                println!("{}: command not found", command)
+            }
+            Err(e) => println!("error {e}"),
         }
-        Err(e) => println!("error {e}"),
     }
 }
